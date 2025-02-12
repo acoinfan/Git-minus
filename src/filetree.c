52e66d4c135559348811391df29bd2c98b9f8db5
a_coin_fan <1770393635@qq.com>
2025-02-12 20:07:12 +0800

    Automatic commit.

--- src/filetree.c
+++ src/filetree.c
@@ -285,7 +285,7 @@ int is_stored(const char *hash, const char *file_PATH){
             return 0;
         }
 
-        snprintf(stored_PATH, FILE_HASH_LEN, "%s%s-%03d", cwd, hash, count);
+        sprintf(stored_PATH, "%s%s-%03d", cwd, hash, count);
         printf("detecting: %s\n", stored_PATH);
         if (!exists(stored_PATH)){
             printf("returning -%d since not exist file\n", count);
